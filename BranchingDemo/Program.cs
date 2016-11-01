ff7878c49d3fb357078b6ca296102f9fafe337ae
Patrick Pan <jinfeng.pan@31huiyi.com>
2016-11-01 13:31:03 +0800

    init

---
+++ BranchingDemo/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace BranchingDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class Account
+    {
+        private bool IsVerified { get; set; }
+        private bool IsClosed { get; set; }
+        public void Deposit(decimal amount)
+        {
+            
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if(!this.IsVerified)
+                return; //or do something else...
+            //Widthdraw money
+            
+        }
+
+        public void HolderVerified()
+        {
+            this.IsVerified = true;
+        }
+
+        public void Close()
+        {
+            this.IsClosed = true;
+        }
+    }
+}
